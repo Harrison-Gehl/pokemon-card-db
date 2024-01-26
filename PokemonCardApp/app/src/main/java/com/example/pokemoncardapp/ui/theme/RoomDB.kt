0befc1aee9b9cdb767480d023e011cce05a459ca
abbi gehlbach <harrison.gehlbach@gmail.com>
2024-01-26 11:07:20 -0500

    switching to sqlite to room db

--- PokemonCardApp/app/src/main/java/com/example/pokemoncardapp/ui/theme/RoomDB.kt
+++ PokemonCardApp/app/src/main/java/com/example/pokemoncardapp/ui/theme/RoomDB.kt
@@ -1,4 +1,29 @@
 package com.example.pokemoncardapp.ui.theme
 
-class RoomDB {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//https://developer.android.com/training/data-storage/room/defining-data
+@Entity(primaryKeys = ["cardNum", "series","lang"])
+data class Cards(
+    // primary key components
+    val cardNum: String?,
+    val series: String?,
+    val lang: String?,
+
+    // info
+    val name: String?,
+    val type: String?,
+    val evolution: String?,
+
+    // misc attributes
+    val normalArt: Int?,
+    val holo: Int?,
+    val reverseHolo: Int?,
+    val fullHolo: Int?,
+
+    val v: Int?,
+    val ex: Int?,
+    val gx: Int?,
+)
+
